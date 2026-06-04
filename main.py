c1612fd2de0cf9e36389d937c5cc739924ab5dbc
shaniad14 <shaniad1414@gmail.com>
2026-06-03 19:47:59 -0700

    Added a function

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import tkinter as tk
 from tkinter import messagebox
 
 # API FUNCTIONS
+
+def get_dog_info():
+    """Gets random dog breed image + breed info"""
+    url = "https://dog.ceo/api/breeds/image/random"
+    response = requests.get(url).json()
+
+    image_url = response["message"]
+
+    # Try to extract breed from URL
+    breed = image_url.split("/")[4].replace("-", " ")
+
