0de7738494b373ec5f60be14abece3b0e73d2f6a
shaniad14 <shaniad1414@gmail.com>
2026-06-03 19:52:15 -0700

    Added more functions

--- main.py
+++ main.py
@@ -14,3 +14,19 @@ def get_dog_info():
     # Try to extract breed from URL
     breed = image_url.split("/")[4].replace("-", " ")
 
+     return {
+        "breed": breed,
+        "image": image_url
+    }
+
+
+def get_fox_info():
+    """Gets random fox image"""
+    url = "https://randomfox.ca/floof/"
+    response = requests.get(url).json()
+
+    return {
+        "image": response["image"]
+    }
+
+
